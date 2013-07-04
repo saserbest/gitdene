c28fcbf3315cf309871f25f6e72450d076ab1a3e
Seyit Ali Serbest <dmadmin@samanyolu.tv>
2013-07-04 11:34:00 +0300

    ilk proje baslatmasi

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Gitdeneme::Application.config.secret_key_base = '2bc9bfd455b3438b2fb1b1c153e0948889118cf66495349543d63ef85f88f0d9e90c2a778181332ca90da29b089a05041212ae9daa9bf17a2f73937b0db0768d'
